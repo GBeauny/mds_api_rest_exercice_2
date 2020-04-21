e118b594a89d8258e8ebab647e685f8edce910dc
Geoffrey <geoffrey.beauny@publicissapient.com>
2020-04-21 08:03:26 +0200

    Adding updates

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const routes = require('./api/route/index');
-
+ 
+ 
+ 
 const app = express();
 const port = process.env.PORT || 3000;
-
+ 
+ 
+ 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
+ 
+  
 routes(app);
 
+ 
 mongoose
-  .connect(
-    'mongodb+srv://test:mds_ws@cluster0-jdmbr.gcp.mongodb.net/test?retryWrites=true&w=majority'
-  )
-  .then(result => {
-    app.listen(port);
-    console.log('Your first node api is running on port: ' + port);
-  })
-  .catch(err => console.log(err));
-
+ .connect(
+ 'mongodb+srv://test:mds_ws@cluster0-jdmbr.gcp.mongodb.net/test?retryWrites=true&w=majority'
+ )
+ .then(result => {
+ app.listen(port);
+ console.log('Your first node api is running on port: ' + port);
+ })
+ .catch(err => console.log(err));
+ 
+ 
+ 
 module.exports = app;
